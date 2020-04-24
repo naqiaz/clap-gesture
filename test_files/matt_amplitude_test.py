b8a07a6eea40bb19043eabab82b81fb1e17cb927
MatthewLeeHan <mlh6fc@virginia.edu>
2020-04-23 21:08:37 -0400

    really simple single clap detection

--- test_files/matt_amplitude_test.py
+++ test_files/matt_amplitude_test.py
@@ -3,9 +3,11 @@ import time
 import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
+import numpy as np
+
 
 #phyphox configuration
-PP_ADDRESS = "http://192.168.50.199:8080" # replace with your address shown in the phyphox app!!!
+PP_ADDRESS = "http://10.0.0.70:8080" # replace with your address shown in the phyphox app!!!
 PP_CHANNELS = ["dB", "time", "mean"]
 
 #global var to save timestamp
@@ -42,14 +44,20 @@ def getData():
 # tracking number of claps
 
 def main():
-    clap_count = 0
+    analyze = [] #list
 
+    clap_count = 0
+    last_time = 0.0
     while True:
         [t, n_db, n_time, n_mean] = getData()
         print('time: ', t, ' // dB: ', n_db, ' // ', n_time, ' // ', n_mean, 'clap_count: ', clap_count)
-        #time.sleep(INTERVALS/1000)   # Delays for INTERVALS seconds.
-        if n_db > -25: # -25 is a random threshold value I chose
-            clap_count += 1
+        # time.sleep(INTERVALS/1000)   # Delays for INTERVALS seconds.
 
+        analyze.append(float(n_db))
+        nums = analyze[-2:-1]
+        if len(nums) >= 1:
+            if nums[0] > -25 and (n_time > last_time + 1.0): # -25 is a random threshold value I chose
+                clap_count += 1
+                last_time = float(n_time)
 if __name__ == '__main__':
     main()
