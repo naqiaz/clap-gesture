4aebcbd7a585dc4343ddd202c835964cef4c1287
naqiaz <62780042+naqiaz@users.noreply.github.com>
2020-04-25 15:52:29 -0400

    Add files via upload

--- clap-gesture.py
+++ clap-gesture.py
@@ -15,7 +15,7 @@ xs = []
 
 # global array to save acceleration
 dB =[]
-time = []
+times = []
 mean = []
 
 # make one of them true at a time
@@ -27,17 +27,17 @@ def getSensorData():
     url = PP_ADDRESS + "/get?" + ("&".join(PP_CHANNELS)) + "=full"
     data = requests.get(url=url).json()
     dB = data["buffer"][PP_CHANNELS[0]]["buffer"][0]
-    time = data["buffer"][PP_CHANNELS[1]]["buffer"][0]
+    times = data["buffer"][PP_CHANNELS[1]]["buffer"][0]
     mean = data["buffer"][PP_CHANNELS[2]]["buffer"][0]
     # print (accX, ' ', accY, ' ', accY)
-    return [dB, time, mean]
+    return [dB, times, mean]
         
 def getData():
     [n_db, n_time, n_mean] = getSensorData() # get nth sample
     t = dt.datetime.now().strftime('%M:%S.%f') #%H:%M:%S.%f
     xs.append(t) 
     dB.append(n_db)
-    time.append(n_time)
+    times.append(n_time)
     mean.append(n_mean)
     return [t, n_db, n_time, n_mean]
     
@@ -46,16 +46,27 @@ def getData():
 def main():
     analyze = [] #list
     clap_count = 0
+    
+    # stores claps recorded per 3 second interval
+    claps = clap_count;
+
+    # time since clap count was updated 
     last_time = 0.0
+    
+    # time since clap count was reset 
     reset_time = 0.0
-    clap_time = 0.0
+    
+    # time since action was updated 
+    action_time = 0.0
+    
     action = ""
     last_action = ""
+    
     while True:
         [t, n_db, n_time, n_mean] = getData()
-        # prints out data collected every 1 seconds
-        # if(n_time > last_time + 1):
-            #print('time: ', n_time, ' // dB: ', n_db, ' // clap_count: ', clap_count, ' // action: ',action)
+        # prints out data collected every 3 seconds
+        # if(n_time > last_time + 3):
+        # print('time: ', n_time, '  dB: ', n_db, ' clap_count: ', clap_count, ' action: ',action)
 
         analyze.append(float(n_db))
         nums = analyze[-2:-1]
@@ -64,43 +75,38 @@ def main():
             # was having trouble with the high resolution of this sensor... it detected 1 clap 10+ times...
             # " n_time > last_time + 0.5 "  #  -> as soon as -40 decibel threshold is passed, waits 0.5 seconds to detect again (to prevent excess detection)
             # seems to work for single and double claps... although may not be 100% accurate 
-            if nums[0] > -40 and (n_time > last_time + 0.5): # -40 is a threshold value that works 
+            if nums[0] > -40  and (n_time > last_time + 0.5): # -40 is a threshold value that works 
                 clap_count += 1
                 last_time = float(n_time)
             
-            # resets the clap counter every two seconds
-            if (n_time > reset_time + 2):
-                reset_time = n_time
-                clap_count = 0
-            
-            # determine which action based on clap count
-            # checks the clap count every 1.5 seconds
-            # 1 - clap -> play/pause
-            # 2 - clap -> rewind
-            # 3+ - clap -> FastForward
-            if (clap_count > 0 and (n_time > clap_time + 1.5)):
-                if (clap_count == 1):
+            # resets the clap counter every 3.0 seconds
+            # stores the total number of claps detected each 3.0 seconds
+            # determine which action based on number of claps detected
+            if (n_time > reset_time + 3.0):
+                reset_time = float(n_time) 
+                claps = clap_count
+                clap_count = 0; 
+                if (claps == 1): # 1  - clap -> play/pause
                     if (action == "play"):
                         action = "pause"
                         last_action = "play"
-                        # print('clap_count: ', clap_count, 'action: ', action, 'last_action: ', last_action)
+                        # print('claps: ', claps, ' action: ', action)
                     else:
                         action = "play"
                         last_action = "pause"
-                        # print('clap_count: ', clap_count, 'action: ', action, 'last_action: ', last_action)
-                if (clap_count == 2):
+                        # print('claps: ', claps, ' action: ', action)
+                if (claps == 2): # 2  - clap -> rewind
                         action = "rewind"
                         last_action = "play"
-                        # print('clap_count: ', clap_count, 'action: ', action, 'last_action: ', last_action)
-                if (clap_count > 2):
+                        # print('claps: ', claps, ' action: ', action)
+                if (claps > 2): # 3+ - clap -> FastForward
                         action = "forward"
                         last_action = "play"
-                        # print('clap_count: ', clap_count, 'action: ', action, 'last_action: ', last_action)
-                clap_time = n_time
-                        
-           # when a new action is determined, the video is manipulated based on that action
+                        # print('claps: ', claps, ' action: ', action)
+            
+            # when a new action is determined, the video is manipulated based on that action
             while (last_action != action):  
                 #Jess's Code
-                action = last_action = play            
+                action = last_action = "play"            
 if __name__ == '__main__':
     main()
